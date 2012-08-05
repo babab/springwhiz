aebb52c86a34cb994bc126c0ca419c0ac743cc71
Benjamin Althues <benjamin@babab.nl>
2012-08-05 22:03:36 +0200

    Add redirection to duckduckgo.com

--- springwhiz/views.py
+++ springwhiz/views.py
@@ -13,10 +13,30 @@
 # You should have received a copy of the GNU General Public License
 # along with springwhiz.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.shortcuts import render_to_response
+from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
+
+def query_handler(query):
+    return redirect('https://duckduckgo.com?q=%s' % query)
 
 def start(request):
     data = {}
+
+    if request.POST:
+        if request.POST.has_key('mode'):
+            mode = request.POST.get('mode')
+
+        if request.POST.has_key('q'):
+            q = request.POST.get('q')
+
+        if mode == 'unset':
+            return query_handler(q)
+        elif mode == 'bang':
+            return query_handler('!' + q)
+        elif mode == 'ddg1st':
+            return query_handler('\\' + q)
+        else:
+            data = {'mode': mode, 'query': q}
+
     context = RequestContext(request)
     return render_to_response('start.html', data, context)
