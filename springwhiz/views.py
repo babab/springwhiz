18e45b221a04423db7bfc9e94967112f0ff6ac4c
Benjamin Althues <benjamin@babab.nl>
2013-01-20 03:03:35 +0100

    Add proper handling of invalid commands

--- springwhiz/views.py
+++ springwhiz/views.py
@@ -19,24 +19,25 @@ from django import forms
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
 from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
 
 from springwhiz.settings import BASE_URL
 
 
-def _query_handler(query):
+def _query_handler(request, query):
     if query[0] == '@':
-        return _command_handler(query[1:])
+        return _command_handler(request, query[1:])
     elif query[0] == '#':
-        return HttpResponse('Bookmarks are not supported yet',
-                            mimetype='text/plain')
+        return render_to_response('index.html',
+                                  {'error': 'Bookmarks are not supported yet',
+                                   'query': query},
+                                  RequestContext(request))
     else:
         return redirect('https://duckduckgo.com?q=%s' % query)
 
 
-def _command_handler(query):
+def _command_handler(request, query):
     if query == 'reg' or query == 'register':
         return redirect(reverse('register'))
     elif query == 'li' or query == 'login':
@@ -46,8 +47,14 @@ def _command_handler(query):
     elif query == 'np' or query == 'notepad':
         return redirect(reverse('notepad'))
     elif query == 'bm' or query == 'bookmark':
-        return HttpResponse('Bookmarks are not supported yet',
-                            mimetype='text/plain')
+        return render_to_response('index.html',
+                                  {'error': 'Bookmarks are not supported yet',
+                                   'query': query},
+                                  RequestContext(request))
+    else:
+        return render_to_response('index.html', {'error': 'Invalid command',
+                                                 'query': query},
+                                  RequestContext(request))
 
 
 def index(request):
@@ -59,15 +66,15 @@ def index(request):
             q = request.POST.get('q')
 
         if mode == 'bang':
-            return _query_handler('!' + q)
+            return _query_handler(request, '!' + q)
         elif mode == 'ddg1st':
-            return _query_handler('\\' + q)
+            return _query_handler(request, '\\' + q)
         elif mode == 'command':
-            return _query_handler('@' + q)
+            return _query_handler(request, '@' + q)
         elif mode == 'bookmark':
-            return _query_handler('#' + q)
+            return _query_handler(request, '#' + q)
         else:
-            return _query_handler(q)
+            return _query_handler(request, q)
 
     return render_to_response('index.html', {}, RequestContext(request))
 
